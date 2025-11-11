5f2b3570deadb897cc14fa4cb01ccca7018e0f8b
Luceta <57941164+Luceta@users.noreply.github.com>
2025-11-12 01:40:25 +0900

    navigation menu

---
+++ app/common/components/navigation.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router";
+import { Separator } from "~/common/components/ui/separator";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+
+const menus = [
+  { name: "Home", to: "/" },
+  { name: "About", to: "/about" },
+  { name: "Contact", to: "/contact" },
+];
+
+export default function Navigation() {
+  return (
+    <nav className="flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50">
+      <div className="flex items-center">
+        <Link to="/" className="font-bold tracking-tighter text-lg">
+          Wishve
+        </Link>
+        <Separator orientation="vertical" className="h-6 mx-4" />
+        <NavigationMenu>
+          <NavigationMenuList>
+            {menus.map((menu) => (
+              <NavigationMenuItem key={menu.name}>
+                <NavigationMenuLink asChild>
+                  <Link to={menu.to}>{menu.name}</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </nav>
+  );
+}
